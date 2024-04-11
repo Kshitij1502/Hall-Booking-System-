bd9a106342d1edae45f835ea35362cf1bde4db05
Kshitij1502 <86653069+Kshitij1502@users.noreply.github.com>
2024-04-11 15:17:15 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,4 @@
-// const mongoose = require("mongoose");
 
-// var mongoDBURL = 'mongodb+srv://Kshitij1502:22706012345@hallbooking.izfabxm.mongodb.net/HallBooking';
-
-// mongoose.connect(mongoDBURL)
-//   .then(() => console.log(`Mongo DB Connection Successful`))
-//   .catch(err => console.error(`Mongo DB Connection Failed: ${err}`));
-
-// module.exports = mongoose;
 const mongoose = require("mongoose");
 
 // MongoDB connection URI
